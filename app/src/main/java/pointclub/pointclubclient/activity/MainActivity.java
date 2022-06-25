cd502d8f8927212ad8333f0ad0bdc6a9c852528a
Tamir Erez <tamirerez3@gmail.com>
2022-06-25 19:46:58 +0300

    Call New RestController

--- app/src/main/java/pointclub/pointclubclient/activity/MainActivity.java
+++ app/src/main/java/pointclub/pointclubclient/activity/MainActivity.java
@@ -2,18 +2,12 @@ package pointclub.pointclubclient.activity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import lombok.SneakyThrows;
 import pointclub.pointclubclient.R;
-import pointclub.pointclubclient.rest.RetrofitClient;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import pointclub.pointclubclient.rest.RestController;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,24 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        test();
+        isServerAlive();
         getToken();
     }
 
-    private void test() {
-        Call<Boolean> call = RetrofitClient.getInstance().getApi().getIsAlive();
-        call.enqueue(new Callback<>() {
-            @Override
-            public void onResponse(@NonNull Call<Boolean> call, @NonNull Response<Boolean> response) {
-                Boolean isAlive = response.body();
-                Toast.makeText(getApplicationContext(), "Is alive: " + isAlive, Toast.LENGTH_LONG).show();
+    private void isServerAlive() {
+        RestController.getInstance().isAlive(booleanResponse -> {
+            if(booleanResponse.isSuccessful()){
+                Log.i("Server Status", "Alive");
             }
-
-            @SneakyThrows
-            @Override
-            public void onFailure(Call<Boolean> call, Throwable t) {
-                //TODO: handle fail to connect to server
-                throw t;
+            else{
+                Log.w("Server Status", "Bad Request");
             }
         });
     }
@@ -51,10 +38,8 @@ public class MainActivity extends AppCompatActivity {
                         return;
                     }
 
-                    // Get new FCM registration token
                     String token = task.getResult();
                     Log.i("Token", token);
-                    Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
                 });
     }
 }
