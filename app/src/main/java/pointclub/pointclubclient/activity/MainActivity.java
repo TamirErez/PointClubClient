6a65e178bb536a2a023a8f639fcc506c10b4feac
Tamir Erez <tamirerez3@gmail.com>
2022-07-02 09:53:06 +0300

    Register User When Opening the App

--- app/src/main/java/pointclub/pointclubclient/activity/MainActivity.java
+++ app/src/main/java/pointclub/pointclubclient/activity/MainActivity.java
@@ -1,21 +1,30 @@
 package pointclub.pointclubclient.activity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessaging;
 
+import androidx.activity.result.ActivityResult;
 import androidx.appcompat.app.AppCompatActivity;
 import pointclub.pointclubclient.R;
 import pointclub.pointclubclient.rest.RestController;
+import pointclub.pointclubclient.service.ActivityLauncherService;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String SERVER_STATUS_TAG = "Server Status";
+    private int userId;
+    protected final ActivityLauncherService<Intent, ActivityResult> activityLauncher = ActivityLauncherService.registerActivityForResult(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setupServer();
+        registerUser();
     }
 
     private void setupServer() {
@@ -47,5 +56,19 @@ public class MainActivity extends AppCompatActivity {
                     String token = task.getResult();
                     Log.i("Token", token);
                 });
+    }
+
+    private void registerUser() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        activityLauncher.launch(intent, result -> {
+            if (result.getResultCode() == Activity.RESULT_OK) {
+                Intent data = result.getData();
+                if (data != null) {
+                    Log.i("Register User", "New User id: " + data.getIntExtra("id", -1));
+                } else {
+                    Log.e("Register User", "Failed to Register User");
+                }
+            }
+        });
     }
 }
