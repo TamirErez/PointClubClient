d5dc45db5f19ea7a0f0f468890d826f6b900a7b2
Tamir Erez <tamirerez3@gmail.com>
2022-07-15 17:52:36 +0300

    Add Users and Messages to Room

--- app/src/main/java/pointclub/pointclubclient/model/Room.java
+++ app/src/main/java/pointclub/pointclubclient/model/Room.java
@@ -1,6 +1,10 @@
 package pointclub.pointclubclient.model;
 
 import com.orm.SugarRecord;
+import com.orm.dsl.Ignore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -13,6 +17,8 @@ public class Room extends SugarRecord<Room> {
 
     private int roomId;
     private String name;
+    @Ignore
+    private List<User> users = new ArrayList<>();
 
     public Room(String name) {
         this.name = name;
@@ -21,5 +27,9 @@ public class Room extends SugarRecord<Room> {
     public Room(int roomId, String name) {
         this.roomId = roomId;
         this.name = name;
+    }
+
+    public List<Message> getMessages() {
+        return Message.find(Message.class, "room = ?", String.valueOf(roomId));
     }
 }
