b01d15cf045d166c2db05caf02614eb3fd5e08ef
Tamir Erez <tamirerez3@gmail.com>
2023-03-31 17:35:47 +0300

    Remove Redundant Intent

--- app/src/main/java/pointclub/pointclubclient/activity/MainActivity.java
+++ app/src/main/java/pointclub/pointclubclient/activity/MainActivity.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-import activity.ChessActivity;
 import androidx.activity.result.ActivityResult;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -42,8 +41,6 @@ public class MainActivity extends AppCompatActivity {
         initRooms();
         initRoomRecycler();
         setRegisterRoomButtonAction();
-
-        Intent i = new Intent(this, ChessActivity.class);
     }
 
     private void setupServer() {
